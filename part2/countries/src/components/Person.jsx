373072a733645fd8127ea260ca142567e45bf1d0
Amplaza-and <albertomplazaperez@gmail.com>
2024-09-06 10:29:44 +0200

    Delete part2/countries/src directory

--- part2/countries/src/components/Person.jsx
+++
@@ -1,9 +0,0 @@
-const Person = ({person, deletePerson}) => {
-      return (    
-        <li>{person.name} {person.number}  
-        <button id={person.id} onClick= {deletePerson} > delete </button>
-        </li>
-      )
-    }
-
-export default Person
